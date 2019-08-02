622bfb0972533c4d56129a3b697b7dc5cf9f754b
Miavega <miguel_9504@hotmail.com>
2019-08-02 16:19:47 -0500

    Se genera el MID API de ARKA II #48

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	_ "github.com/udistrital/arka_mid/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
+	"github.com/udistrital/utils_oas/responseformat"
+)
+
+func main() {
+	beego.BConfig.RecoverFunc = responseformat.GlobalResponseHandler
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	beego.Run()
+}
