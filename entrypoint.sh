2ea0e9bf865e7576f78bfc558fa8ec61f08b85ba
fernandotower <fernandotower@gmail.com>
2019-09-17 12:11:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
-  export ACTA_RECIBIDO_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/arka_mid/db/username --output text --query Parameter.Value)"
-  export ACTA_RECIBIDO_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/arka_mid/db/password --output text --query Parameter.Value)"
-fi
+#if [ -n "${PARAMETER_STORE:-}" ]; then
+  #export ACTA_RECIBIDO_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/arka_mid/db/username --output text --query Parameter.Value)"
+  #export ACTA_RECIBIDO_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/arka_mid/db/password --output text --query Parameter.Value)"
+#fi
 
 exec ./main "$@"
