5e73150220fec3fea7054a55bb0f78effc2f5266
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-04-11 01:15:28 -0500

    fix: Se agrega el defer a la función GetCuentaContable y se remueven los fmt.Println innecesarios

--- helpers/cuentasContablesHelper/cuentasContables.helper.go
+++ helpers/cuentasContablesHelper/cuentasContables.helper.go
@@ -1,26 +1,33 @@
 package cuentasContablesHelper
 
 import (
-	"fmt"
-
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 
-	// "github.com/udistrital/arka_mid/models"
 	"github.com/udistrital/utils_oas/request"
 )
 
 // GetCuentaContable ...
 func GetCuentaContable(cuentaContableId string) (cuentaContable map[string]interface{}, outputError map[string]interface{}) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			outputError = map[string]interface{}{
+				"funcion": "GetCuentaContableByCodigo - Unhandled Error!",
+				"err":     err,
+				"status":  "500",
+			}
+			panic(outputError)
+		}
+	}()
+
 	var (
 		urlcrud string
 	)
-	fmt.Println(cuentaContableId)
 	urlcrud = "http://" + beego.AppConfig.String("cuentasContablesService") + "cuenta_contable/" + cuentaContableId
 
 	if response, err := request.GetJsonTest(urlcrud, &cuentaContable); err == nil { // (2) error servicio caido
 		if response.StatusCode == 200 { // (3) error estado de la solicitud
-			fmt.Print(cuentaContable)
 			return cuentaContable, nil
 		} else {
 			logs.Info("Error (3) estado de la solicitud")
