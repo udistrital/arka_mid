49fb46725b37210325f077e203305c15e493a338
Juan David Velez Alvarez <juan.velez@and.gov.co>
2021-12-27 09:51:19 -0500

    fix: manejo error parametrización cuentas. retorna id tercero. Refactor contab

--- helpers/catalogoElementosHelper/catalogoElementos.helper.go
+++ helpers/catalogoElementosHelper/catalogoElementos.helper.go
@@ -56,6 +56,61 @@ func GetCatalogoById(catalogoId int) (catalogo *[]map[string]interface{}, output
 	}
 }
 
+func GetInfoSubgrupo(subgrupoId int) (detalleSubgrupo map[string]interface{}, outputError map[string]interface{}) {
+	defer func() {
+		if err := recover(); err != nil {
+			outputError = map[string]interface{}{
+				"funcion": "GetInfoSubgrupo - Unhandled Error!",
+				"err":     err,
+				"status":  "500",
+			}
+			panic(outputError)
+		}
+	}()
+
+	if subgrupoId <= 0 {
+		err := fmt.Errorf("subgrupoId MUST be > 0 - Got: %d", subgrupoId)
+		logs.Error(err)
+		outputError = map[string]interface{}{
+			"funcion": "GetInfoSubgrupo - subgrupoId <= 0",
+			"err":     err,
+			"status":  "400",
+		}
+		panic(outputError)
+	}
+
+	var detalles []map[string]interface{}
+
+	urlcrud := "http://" + beego.AppConfig.String("catalogoElementosService") + "subgrupo?limit=-1"
+	urlcrud += "&query=Activo:True&Id:" + strconv.Itoa(int(subgrupoId))
+
+	if response, err := request.GetJsonTest(urlcrud, &detalles); err == nil && response.StatusCode == 200 { // (2) error servicio caido
+		// fmt.Println(cuentasSubgrupo[0])
+		if detalles[0]["Id"].(int) != 0 {
+			return detalles[0], nil
+		} else {
+			err = fmt.Errorf("Cuenta no existe")
+			outputError = map[string]interface{}{
+				"funcion": "GetInfoSubgrupo - request.GetJsonTest(urlcrud, &cuentasSubgrupo)",
+				"err":     err,
+				"status:": "502",
+			}
+			return nil, outputError
+		}
+	} else {
+		if err == nil {
+			err = fmt.Errorf("Undesired Status Code: %d", response.StatusCode)
+		}
+		logs.Error(err)
+		outputError = map[string]interface{}{
+			"funcion": "GetInfoSubgrupo - request.GetJsonTest(urlcrud, &cuentasSubgrupo)",
+			"err":     err,
+			"status:": "502",
+		}
+		return nil, outputError
+	}
+}
+
 // GetCuentasContablesGrupo ...
 func GetCuentasContablesSubgrupo(subgrupoId int) (cuentasSubgrupoTransaccion []map[string]interface{}, outputError map[string]interface{}) {
 
