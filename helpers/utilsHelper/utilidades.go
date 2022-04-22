6c1e07bfaad3bd528f17996328f5f5bee7e872ea
Alex Bustos <alexfbp@gmail.com>
2022-04-21 19:34:30 -0500

    feat: partir utils, 2/2

--- helpers/utilsHelper/utilidades.go
+++ helpers/utilsHelper/utilidades.go
@@ -3,7 +3,6 @@ package utilsHelper
 import (
 	"encoding/json"
 	"fmt"
-	"net/url"
 	"strings"
 
 	"github.com/astaxie/beego/logs"
@@ -187,38 +186,4 @@ func RemoveDuplicateIds(addrs []int) []int {
 		}
 	}
 	return result
-}
-
-func EncodeUrl(query string, fields string, sortby string, order string, offset string, limit string) string {
-	params := url.Values{}
-
-	if len(query) > 0 {
-		params.Add("query", query)
-	}
-
-	if len(fields) > 0 {
-		params.Add("fields", fields)
-	}
-
-	if len(sortby) > 0 {
-		params.Add("sortby", sortby)
-
-	}
-
-	if len(order) > 0 {
-		params.Add("order", order)
-
-	}
-
-	if len(offset) > 0 {
-		params.Add("offset", offset)
-
-	}
-
-	if len(limit) > 0 {
-		params.Add("limit", limit)
-
-	}
-
-	return params.Encode()
 }
