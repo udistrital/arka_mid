76a8f7a3e18b3b6c8bcbf4147da2940be3348036
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-04-27 22:09:17 -0500

    feat udistrital/arka_cliente#854: Hallazgos
    - Unmarshal en package utils.

---
+++ helpers/utilsHelper/Unmarshal.go
@@ -0,0 +1,23 @@
+package utilsHelper
+
+import (
+	"encoding/json"
+
+	"github.com/astaxie/beego/logs"
+	"github.com/udistrital/utils_oas/errorctrl"
+)
+
+func Unmarshal(in string, out interface{}) (outputError map[string]interface{}) {
+
+	funcion := "Unmarshal"
+	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+
+	if err := json.Unmarshal([]byte(in), &out); err != nil {
+		logs.Error(err)
+		eval := " - json.Unmarshal([]byte(int), &out)"
+		return errorctrl.Error(funcion+eval, err, "500")
+	}
+
+	return
+
+}
