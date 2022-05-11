425a71886d52352b3662d252e7ae02b4b1892bfa
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-05-10 22:32:37 -0500

    feat udistrital/arka_cliente#854: Hallazgos
    - Refactor operaciones crud api acta_recibido_crud.

--- helpers/crud/actaRecibido/actaRecibido.go
+++ helpers/crud/actaRecibido/actaRecibido.go
@@ -11,15 +11,18 @@ import (
 	"github.com/udistrital/utils_oas/request"
 )
 
+var path = "http://" + beego.AppConfig.String("actaRecibidoService")
+
 // GetElementoById consulta controlador elemento/{id} del api acta_recibido_crud
 func GetElementoById(id int) (elemento *models.Elemento, outputError map[string]interface{}) {
 
-	funcion := "GetElementoById"
-	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+	funcion := "GetElementoById - "
+	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "elemento/" + strconv.Itoa(id)
+	urlcrud := path + "elemento/" + strconv.Itoa(id)
 	if err := request.GetJson(urlcrud, &elemento); err != nil {
-		eval := " - request.GetJson(urlcrud, &elemento)"
+		logs.Error(urlcrud+", ", err)
+		eval := "request.GetJson(urlcrud, &elemento)"
 		return nil, errorctrl.Error(funcion+eval, err, "502")
 	}
 
@@ -29,12 +32,13 @@ func GetElementoById(id int) (elemento *models.Elemento, outputError map[string]
 // GetAllElemento query controlador elemento del api acta_recibido_crud
 func GetAllElemento(query string, fields string, sortby string, order string, offset string, limit string) (elementos []*models.Elemento, outputError map[string]interface{}) {
 
-	funcion := "GetAllElemento"
-	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+	funcion := "GetAllElemento - "
+	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "elemento?" + utilsHelper.EncodeUrl(query, fields, sortby, order, offset, limit)
+	urlcrud := path + "elemento?" + utilsHelper.EncodeUrl(query, fields, sortby, order, offset, limit)
 	if err := request.GetJson(urlcrud, &elementos); err != nil {
-		eval := " - request.GetJson(urlcrud, &elementos)"
+		logs.Error(urlcrud+", ", err)
+		eval := "request.GetJson(urlcrud, &elementos)"
 		return nil, errorctrl.Error(funcion+eval, err, "502")
 	}
 
@@ -44,12 +48,13 @@ func GetAllElemento(query string, fields string, sortby string, order string, of
 // GetAllHistoricoActa query controlador historico_acta del api acta_recibido_crud
 func GetAllHistoricoActa(query string, fields string, sortby string, order string, offset string, limit string) (historicos []*models.HistoricoActa, outputError map[string]interface{}) {
 
-	funcion := "GetAllHistoricoActa"
-	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+	funcion := "GetAllHistoricoActa - "
+	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "historico_acta?" + utilsHelper.EncodeUrl(query, fields, sortby, order, offset, limit)
+	urlcrud := path + "historico_acta?" + utilsHelper.EncodeUrl(query, fields, sortby, order, offset, limit)
 	if err := request.GetJson(urlcrud, &historicos); err != nil {
-		eval := " - request.GetJson(urlcrud, &historicos)"
+		logs.Error(urlcrud+", ", err)
+		eval := "request.GetJson(urlcrud, &historicos)"
 		return nil, errorctrl.Error(funcion+eval, err, "502")
 	}
 
@@ -59,12 +64,13 @@ func GetAllHistoricoActa(query string, fields string, sortby string, order strin
 // PutElemento put controlador elemento del api acta_recibido_crud
 func PutElemento(elemento *models.Elemento, elementoId int) (elemento_ *models.Elemento, outputError map[string]interface{}) {
 
-	funcion := "PutElemento"
-	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+	funcion := "PutElemento - "
+	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "elemento/" + strconv.Itoa(elementoId)
+	urlcrud := path + "elemento/" + strconv.Itoa(elementoId)
 	if err := request.SendJson(urlcrud, "PUT", &elemento_, &elemento); err != nil {
-		eval := ` - request.SendJson(urlcrud, "PUT", &elemento_, &elemento)`
+		logs.Error(urlcrud+", ", err)
+		eval := `request.SendJson(urlcrud, "PUT", &elemento_, &elemento)`
 		return nil, errorctrl.Error(funcion+eval, err, "502")
 	}
 
@@ -78,8 +84,9 @@ func GetSoporteById(id int, soporte *models.SoporteActa) (outputError map[string
 	funcion := "GetSoporteById"
 	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "soporte_acta/" + strconv.Itoa(id)
+	urlcrud := path + "soporte_acta/" + strconv.Itoa(id)
 	if err := request.GetJson(urlcrud, &soporte); err != nil {
+		logs.Error(urlcrud+", ", err)
 		eval := " - request.GetJson(urlcrud, &soporte)"
 		return errorctrl.Error(funcion+eval, err, "502")
 	}
@@ -93,7 +100,7 @@ func GetTransaccionActaRecibidoById(id int, transaccion *models.TransaccionActaR
 	funcion := "GetTransaccionActaRecibidoById - "
 	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "transaccion_acta_recibido/" + strconv.Itoa(id)
+	urlcrud := path + "transaccion_acta_recibido/" + strconv.Itoa(id)
 	if err := request.GetJson(urlcrud, &transaccion); err != nil {
 		logs.Error(err)
 		eval := `request.GetJson(urlcrud, &transaccion)`
@@ -109,7 +116,7 @@ func PutTransaccionActaRecibido(id int, transaccion *models.TransaccionActaRecib
 	funcion := "PutTransaccionActaRecibido - "
 	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	urlcrud := "http://" + beego.AppConfig.String("actaRecibidoService") + "transaccion_acta_recibido/" + strconv.Itoa(id)
+	urlcrud := path + "transaccion_acta_recibido/" + strconv.Itoa(id)
 	if err := request.SendJson(urlcrud, "PUT", &transaccion, &transaccion); err != nil {
 		logs.Error(err)
 		eval := `request.SendJson(urlcrud, "PUT", &transaccion, &transaccion)`
