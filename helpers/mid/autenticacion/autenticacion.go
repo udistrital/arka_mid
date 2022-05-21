582bfd30f21106e1f15bcf99bc7b6b5281afad5a
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-05-21 00:38:54 -0500

    feat udistrital/arka_cliente#926: Refactor Traslados
    - Refactor consulta de roles usuario.

--- helpers/mid/autenticacion/autenticacion.go
+++ helpers/mid/autenticacion/autenticacion.go
@@ -4,38 +4,27 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/udistrital/arka_mid/models"
+	"github.com/udistrital/utils_oas/errorctrl"
 	"github.com/udistrital/utils_oas/request"
 )
+
+var basePath = "http://" + beego.AppConfig.String("autenticacionService")
 
 // DataUsuario Consulta datos asociados a un usuario de la MID API de Autenticación
 func DataUsuario(usuarioWSO2 string) (dataUsuario models.UsuarioAutenticacion, outputError map[string]interface{}) {
 
-	defer func() {
-		if err := recover(); err != nil {
-			outputError = map[string]interface{}{
-				"funcion": "DataUsuario - Unhandled Error!",
-				"err":     err,
-				"status":  "500",
-			}
-			panic(outputError)
-		}
-	}()
+	funcion := "DataUsuario - "
+	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
 
-	url := "http://" + beego.AppConfig.String("autenticacionService") + "token/userRol"
-
+	url := basePath + "token/userRol"
 	req := models.UsuarioDataRequest{User: usuarioWSO2}
-
 	if err := request.SendJson(url, "POST", &dataUsuario, &req); err == nil {
 		return dataUsuario, nil
 	} else {
 		var empty models.UsuarioAutenticacion
 		logs.Error(err)
-		outputError = map[string]interface{}{
-			"funcion": "DataUsuario - request.SendJson(url, \"POST\", &dataUsuario, &req)",
-			"err":     err,
-			"status":  "502",
-		}
-		return empty, outputError
+		eval := `request.SendJson(url, "POST", &dataUsuario, &req)`
+		return empty, errorctrl.Error(funcion+eval, err, "500")
 	}
 
 }
