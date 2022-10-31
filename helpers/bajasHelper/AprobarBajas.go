4cfa78b66b7a77a98870685e859ed800b25badac
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2022-10-31 00:24:24 -0500

    feat #266: Umbrales - bajas
    - Construcción de movimientos contables en la aprobación de las bajas, de acuerdo a los umbrales.

--- helpers/bajasHelper/AprobarBajas.go
+++ helpers/bajasHelper/AprobarBajas.go
@@ -2,15 +2,14 @@ package bajasHelper
 
 import (
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/udistrital/arka_mid/helpers/asientoContable"
-	"github.com/udistrital/arka_mid/helpers/catalogoElementosHelper"
 	"github.com/udistrital/arka_mid/helpers/crud/actaRecibido"
-	"github.com/udistrital/arka_mid/helpers/crud/cuentasContables"
+	"github.com/udistrital/arka_mid/helpers/crud/catalogoElementos"
 	"github.com/udistrital/arka_mid/helpers/crud/movimientosArka"
-	"github.com/udistrital/arka_mid/helpers/crud/parametros"
-	crudTerceros "github.com/udistrital/arka_mid/helpers/crud/terceros"
+	"github.com/udistrital/arka_mid/helpers/crud/terceros"
 	"github.com/udistrital/arka_mid/helpers/depreciacionHelper"
 	"github.com/udistrital/arka_mid/helpers/mid/movimientosContables"
 	"github.com/udistrital/arka_mid/helpers/utilsHelper"
@@ -19,257 +18,223 @@ import (
 )
 
 // AprobarBajas Aprobación masiva de bajas: transacciones contables, actualización de movmientos y registro de novedades
-func AprobarBajas(data *models.TrRevisionBaja) (ids []int, outputError map[string]interface{}) {
+func AprobarBajas(data *models.TrRevisionBaja, response *models.ResultadoMovimiento) (outputError map[string]interface{}) {
 
-	funcion := "AprobarBajas"
-	defer errorctrl.ErrorControlFunction(funcion+" - Unhandled Error!", "500")
+	defer errorctrl.ErrorControlFunction("AprobarBajas - Unhandled Error!", "500")
 
 	var (
-		bajas               map[int]models.Movimiento
-		elementosMovimiento map[int]models.ElementosMovimiento
-		novedades           map[int]models.NovedadElemento
-		elementosActa       map[int]models.Elemento
-		cuentasBaja         map[int]models.CuentaSubgrupo
-		cuentasDp           map[int]models.CuentaSubgrupo
-		cuentasAm           map[int]models.CuentaSubgrupo
-		detalleCuentas      map[string]models.CuentaContable
-		detalleSubgrupos    map[int]models.DetalleSubgrupo
-		detalleMediciones   map[int]models.FormatoDepreciacion
-		detalleBajas        map[int]models.FormatoBaja
 		movBj, movDp, movAm int
-		parDebito           int
-		parCredito          int
-		comprobanteID       string
-		query               string
 		terceroUD           int
+		bajas               []*models.Movimiento
 	)
 
 	if err := movimientosArka.GetFormatoTipoMovimientoIdByCodigoAbreviacion(&movBj, "BJ_HT"); err != nil {
-		return nil, err
+		return err
 	}
+
 	if err := movimientosArka.GetFormatoTipoMovimientoIdByCodigoAbreviacion(&movDp, "DEP"); err != nil {
-		return nil, err
+		return err
 	}
+
 	if err := movimientosArka.GetFormatoTipoMovimientoIdByCodigoAbreviacion(&movAm, "AMT"); err != nil {
-		return nil, err
+		return err
 	}
 
-	if db_, cr_, err := parametros.GetParametrosDebitoCredito(); err != nil {
-		return nil, err
+	if UD, err := terceros.GetTerceroUD(); err != nil {
+		return err
+	} else if UD == 0 {
+		response.Error = "No se pudo consultar el tercero para asociar a la transacción contable. Contacte soporte."
+		return
 	} else {
-		parDebito = db_
-		parCredito = cr_
+		terceroUD = UD
 	}
 
-	if err := cuentasContables.GetComprobante(getTipoComprobanteBajas(), &comprobanteID); err != nil {
-		return nil, err
-	}
+	var (
+		bufferCuentas     = make(map[string]models.CuentaContable)
+		detalleSubgrupos  = make(map[int]models.DetalleSubgrupo)
+		detalleMediciones = make(map[int]models.FormatoDepreciacion)
+		detalleBajas      = make(map[int]models.FormatoBaja)
+	)
 
-	query = "query=TipoDocumentoId__Nombre:NIT,Numero:" + crudTerceros.GetDocUD()
-	if terceroUD_, err := crudTerceros.GetAllDatosIdentificacion(query); err != nil {
-		return nil, err
+	if bajas_, err := movimientosArka.GetAllMovimiento(payloadBajas(data.Bajas)); err != nil {
+		return err
+	} else if len(bajas_) == len(data.Bajas) {
+		bajas = bajas_
 	} else {
-		terceroUD = terceroUD_[0].TerceroId.Id
+		response.Error = "No se pudo consultar el detalle de las bajas a aprobar. Contacte soporte."
+		return
 	}
 
-	detalleCuentas = make(map[string]models.CuentaContable)
-	cuentasBaja, cuentasDp, cuentasAm = make(map[int]models.CuentaSubgrupo), make(map[int]models.CuentaSubgrupo), make(map[int]models.CuentaSubgrupo)
-
-	// Paso 1: Consulta los movimientos
-	query = "fields=Detalle,Id,FechaCreacion&limit=-1&query=Id__in:"
-	query += url.QueryEscape(utilsHelper.ArrayToString(data.Bajas, "|"))
-	if bajas_, err := movimientosArka.GetAllMovimiento(query); err != nil {
-		return nil, err
-	} else {
-
-		bajas = make(map[int]models.Movimiento)
-		detalleBajas = make(map[int]models.FormatoBaja)
-		for _, baja := range bajas_ {
-
-			var detalle models.FormatoBaja
-			if err := utilsHelper.Unmarshal(baja.Detalle, &detalle); err != nil {
-				return nil, err
-			}
-
-			bajas[baja.Id] = *baja
-			detalleBajas[baja.Id] = detalle
-
-			ids = append(ids, detalle.Elementos...)
-		}
-	}
-
-	// Paso 2: Consulta los elementos
-	query = "limit=-1&fields=Id,ElementoActaId,ValorTotal,ValorResidual,VidaUtil,MovimientoId&sortby=ElementoActaId&order=desc"
-	query += "&query=Id__in:" + url.QueryEscape(utilsHelper.ArrayToString(ids, "|"))
-	if elementos_, err := movimientosArka.GetAllElementosMovimiento(query); err != nil {
-		return nil, err
-	} else {
-		elementosMovimiento = make(map[int]models.ElementosMovimiento)
-		for _, el := range elementos_ {
-			elementosMovimiento[el.Id] = *el
-		}
-	}
-
-	// Paso 3: Consulta las novedades
-	query = "limit=-1&sortby=MovimientoId,FechaCreacion&order=asc,asc&query=Activo:true,ElementoMovimientoId__Id__in:"
-	query += utilsHelper.ArrayToString(ids, "|")
-	if novedades_, err := movimientosArka.GetAllNovedadElemento(query); err != nil {
-		return nil, err
-	} else {
-		novedades = make(map[int]models.NovedadElemento)
-		for _, nov := range novedades_ {
-			novedades[nov.ElementoMovimientoId.Id] = *nov
+	for _, baja := range bajas {
+		var detalle models.FormatoBaja
+		if err := utilsHelper.Unmarshal(baja.Detalle, &detalle); err != nil {
+			return err
 		}
 
+		detalleBajas[baja.Id] = detalle
 	}
 
-	// Paso 4: Consulta los elementos del acta
-	ids = []int{}
-	for _, el := range elementosMovimiento {
-		ids = append(ids, el.ElementoActaId)
-	}
-
-	fields := "Id,SubgrupoCatalogoId"
-	query = "Id__in:" + utilsHelper.ArrayToString(ids, "|")
-	if elementos_, err := actaRecibido.GetAllElemento(query, fields, "", "", "", "-1"); err != nil {
-		return nil, err
-	} else {
-		elementosActa = make(map[int]models.Elemento)
-		for _, el_ := range elementos_ {
-			elementosActa[el_.Id] = *el_
-		}
-
-	}
-
-	ids = []int{}
-	for _, el := range elementosActa {
-		ids = append(ids, el.SubgrupoCatalogoId)
-	}
-
-	detalleSubgrupos = make(map[int]models.DetalleSubgrupo)
-	if err := catalogoElementosHelper.GetDetalleSubgrupos(ids, detalleSubgrupos); err != nil {
-		return nil, err
-	}
-
-	// Paso 5: Calcula el valor de la transaccion contable para cada baja (si aplica)
-	detalleMediciones = make(map[int]models.FormatoDepreciacion)
 	for _, baja := range bajas {
 
-		movimientos := make([]*models.MovimientoTransaccion, 0)
-		for _, el := range detalleBajas[baja.Id].Elementos {
-			var (
-				sg            int
-				detalleSg     models.DetalleSubgrupo
-				ref           time.Time
-				valorPresente float64
-				valorResidual float64
-				vidaUtil      float64
-				depreciacion  float64
-				amortizacion  float64
-				gasto         float64
-			)
+		var (
+			ids                 []int
+			detalleBaja         models.FormatoBaja
+			elementosMovimiento []*models.ElementosMovimiento
+			elementosActa       = make(map[int]models.Elemento)
+			bajas               []*models.Elemento
+			depreciaciones      []*models.Elemento
+			amortizaciones      []*models.Elemento
+			transaccion         models.TransaccionMovimientos
+		)
 
-			if val, ok := elementosActa[elementosMovimiento[el].ElementoActaId]; ok {
-				sg = val.SubgrupoCatalogoId
+		detalleBaja = detalleBajas[baja.Id]
+		if elementos_, err := movimientosArka.GetAllElementosMovimiento(payloadElementosMovimiento(detalleBaja.Elementos)); err != nil {
+			return err
+		} else if len(elementos_) == len(detalleBaja.Elementos) {
+			elementosMovimiento = elementos_
+			for _, el := range elementos_ {
+				ids = append(ids, el.ElementoActaId)
+			}
+		} else {
+			response.Error = "No se pudo consultar el detalle de los elementos. Contacte soporte."
+			return
+		}
+
+		if elementos_, err := actaRecibido.GetAllElemento(payloadElementos(ids), fieldsElementos(), "", "", "", "-1"); err != nil {
+			return err
+		} else if len(elementos_) == len(detalleBaja.Elementos) {
+			for _, el := range elementos_ {
+				elementosActa[el.Id] = *el
+			}
+		} else {
+			response.Error = "No se pudo consultar el detalle de los elementos. Contacte soporte."
+			return
+		}
+
+		for _, el := range elementosMovimiento {
+
+			acta := elementosActa[el.ElementoActaId]
+			var novedad *models.NovedadElemento
+
+			if _, ok := detalleSubgrupos[acta.SubgrupoCatalogoId]; !ok {
+				if detalle, err := catalogoElementos.GetAllDetalleSubgrupo(getPayloadDetalleSubgrupo(acta.SubgrupoCatalogoId)); err != nil {
+					return err
+				} else if len(detalle) == 1 {
+					detalleSubgrupos[acta.SubgrupoCatalogoId] = *detalle[0]
+				} else {
+					response.Error = "No se pudo la parametrización de los elementos. Contacte soporte."
+					return
+				}
 			}
 
-			if val, ok := detalleSubgrupos[sg]; ok {
-				detalleSg = val
-			}
-
-			if nov, ok := novedades[el]; ok && nov.ValorLibros > 0 {
-
-				if _, ok := detalleMediciones[nov.MovimientoId.Id]; !ok {
-					if dt_, err := depreciacionHelper.GetDetalleDepreciacion(nov.MovimientoId.Detalle); err != nil {
-						return nil, err
-					} else {
-						detalleMediciones[nov.MovimientoId.Id] = *dt_
+			subgrupo := detalleSubgrupos[acta.SubgrupoCatalogoId]
+			if novedad_, err := movimientosArka.GetAllNovedadElemento(payloadNovedad(el.Id)); err != nil {
+				return err
+			} else if len(novedad_) == 1 {
+				novedad = novedad_[0]
+				if _, ok := detalleMediciones[novedad.MovimientoId.Id]; !ok {
+					var detalle models.FormatoDepreciacion
+					if err := utilsHelper.Unmarshal(novedad.MovimientoId.Detalle, &detalle); err != nil {
+						return err
 					}
+					detalleMediciones[novedad.MovimientoId.Id] = detalle
+				}
+			}
+
+			if subgrupo.Amortizacion || subgrupo.Depreciacion {
+
+				var (
+					ref           time.Time
+					valorPresente float64
+					valorResidual float64
+					vidaUtil      float64
+				)
+
+				if novedad != nil {
+					if novedad.ValorLibros-novedad.ValorResidual > 0 {
+						ref, _ = time.Parse("2006-01-02", detalleMediciones[novedad.MovimientoId.Id].FechaCorte)
+						valorPresente = novedad.ValorLibros
+						valorResidual = novedad.ValorResidual
+						vidaUtil = novedad.VidaUtil
+						acta.ValorTotal = novedad.ValorLibros
+					} else if novedad.ValorLibros > 0 { // Ya fue depreciado totalmente pero queda el valor residual
+						baja_ := acta
+						baja_.ValorTotal = novedad.ValorLibros
+						bajas = append(bajas, &acta)
+						continue
+					} else { // Ya fue depreciado totalmente. No hay afectaciones contables.
+						continue
+					}
+				} else { // No hay novedad pero se debe depreciar
+					ref = el.MovimientoId.FechaModificacion
+					valorPresente = el.ValorTotal
+					valorResidual = el.ValorResidual
+					vidaUtil = el.VidaUtil
 				}
 
-				ref, _ = time.Parse("2006-01-02", detalleMediciones[nov.MovimientoId.Id].FechaCorte)
-				valorPresente = nov.ValorLibros
-				valorResidual = nov.ValorResidual
-				vidaUtil = nov.VidaUtil
-
-			} else if !ok && elementosMovimiento[el].ValorTotal > 0 {
-
-				ref = elementosMovimiento[el].MovimientoId.FechaModificacion
-				valorPresente = elementosMovimiento[el].ValorTotal
-				valorResidual = elementosMovimiento[el].ValorResidual
-				vidaUtil = elementosMovimiento[el].VidaUtil
-
-			} else {
-				continue
-			}
-
-			if detalleSg.Amortizacion || detalleSg.Depreciacion {
 				valorMedicion, _ := depreciacionHelper.CalculaDp(
 					valorPresente,
 					valorResidual,
 					vidaUtil,
 					ref.AddDate(0, 0, 1),
-					baja.FechaCreacion)
+					baja.FechaCreacion.Local())
 
 				if valorMedicion > 0 {
-					if detalleSg.Depreciacion {
-						depreciacion = valorMedicion
-						asientoContable.GetInfoContableSubgrupos(movDp, []int{sg}, cuentasDp, detalleCuentas)
-					} else if detalleSg.Amortizacion {
-						amortizacion = valorMedicion
-						asientoContable.GetInfoContableSubgrupos(movAm, []int{sg}, cuentasAm, detalleCuentas)
+					medicion_ := acta
+					medicion_.ValorTotal = valorMedicion
+					acta.ValorTotal -= valorMedicion
+
+					if subgrupo.Depreciacion {
+						depreciaciones = append(depreciaciones, &medicion_)
+					} else if subgrupo.Amortizacion {
+						amortizaciones = append(amortizaciones, &medicion_)
 					}
 				}
-
-				if valorPresente-valorMedicion > 0 {
-					gasto = valorPresente - valorMedicion
-					asientoContable.GetInfoContableSubgrupos(movBj, []int{sg}, cuentasBaja, detalleCuentas)
-				}
-
+				bajas = append(bajas, &acta)
 			} else {
-				gasto = valorPresente
-				asientoContable.GetInfoContableSubgrupos(movBj, []int{sg}, cuentasBaja, detalleCuentas)
+				bajas = append(bajas, &acta)
 			}
-
-			movimientosContablesBaja(cuentasBaja, cuentasDp, cuentasAm, gasto, depreciacion, amortizacion, sg, parCredito, parDebito, terceroUD, detalleCuentas, &movimientos)
 
 		}
 
-		if len(movimientos) > 0 {
-			transaccion := *new(models.TransaccionMovimientos)
+		if msg, err := asientoContable.CalcularMovimientosContables(bajas, descBaja(), movBj, terceroUD, terceroUD, bufferCuentas, detalleSubgrupos, &transaccion.Movimientos); err != nil || msg != "" {
+			response.Error = msg
+			return err
+		}
 
-			if comprobanteID != "" {
-				etiquetas := *new(models.Etiquetas)
-				etiquetas.ComprobanteId = comprobanteID
-				if err := utilsHelper.Marshal(etiquetas, &transaccion.Etiquetas); err != nil {
-					return nil, err
-				}
-			} else {
-				transaccion.Etiquetas = ""
+		if msg, err := asientoContable.CalcularMovimientosContables(depreciaciones, descMovDp(), movDp, terceroUD, terceroUD, bufferCuentas, detalleSubgrupos, &transaccion.Movimientos); err != nil || msg != "" {
+			response.Error = msg
+			return err
+		}
+
+		if msg, err := asientoContable.CalcularMovimientosContables(amortizaciones, descMovAm(), movAm, terceroUD, terceroUD, bufferCuentas, detalleSubgrupos, &transaccion.Movimientos); err != nil || msg != "" {
+			response.Error = msg
+			return err
+		}
+
+		if len(transaccion.Movimientos) > 0 {
+			if msg, err := asientoContable.CreateTransaccionContable(getTipoComprobanteBajas(), "Baja de elementos almacén.", &transaccion); err != nil || msg != "" {
+				response.Error = msg
+				return err
 			}
 
-			transaccion.Activo = true
-			transaccion.ConsecutivoId = detalleBajas[baja.Id].ConsecutivoId
-			transaccion.Descripcion = "Baja de elementos"
-			transaccion.FechaTransaccion = time.Now()
-			transaccion.Movimientos = movimientos
-
+			transaccion.ConsecutivoId = detalleBaja.ConsecutivoId
 			if _, err := movimientosContables.PostTrContable(&transaccion); err != nil {
-				return nil, err
+				return err
 			}
 		}
 
 		data_ := data
 		data_.Bajas = []int{baja.Id}
 		if ids_, err := movimientosArka.PutRevision(data_); err != nil {
-			return nil, err
+			return err
 		} else {
 			ids = ids_
 		}
 
 	}
 
-	return ids, nil
+	return
 }
 
 func GetTerceroIdEncargado(elementoId int, terceroId *int) (outputError map[string]interface{}) {
@@ -344,6 +309,36 @@ func descMovAm() string {
 	return "Amortización restante en baja de elementos"
 }
 
+func descBaja() string {
+	return "Baja de elementos de almacén."
+}
+
 func getTipoComprobanteBajas() string {
 	return "H23"
+}
+
+func payloadBajas(ids []int) string {
+	return "fields=Detalle,Id,FechaCreacion&limit=-1&query=Id__in:" + url.QueryEscape(utilsHelper.ArrayToString(ids, "|"))
+}
+
+func payloadElementosMovimiento(elementos []int) string {
+	return "limit=-1&fields=Id,ElementoActaId,ValorTotal,ValorResidual,VidaUtil,MovimientoId&sortby=ElementoActaId&order=desc&query=Id__in:" +
+		url.QueryEscape(utilsHelper.ArrayToString(elementos, "|"))
+}
+
+func payloadElementos(elementos []int) string {
+	return "Id__in:" + utilsHelper.ArrayToString(elementos, "|")
+}
+
+func fieldsElementos() string {
+	return "Id,SubgrupoCatalogoId,TipoBienId,ValorUnitario,ValorTotal"
+}
+
+func getPayloadDetalleSubgrupo(id int) string {
+	return "limit=1&fields=SubgrupoId,TipoBienId,Amortizacion,Depreciacion&sortby=FechaCreacion&order=desc&query=Activo:true,SubgrupoId__Id:" +
+		strconv.Itoa(id)
+}
+
+func payloadNovedad(id int) string {
+	return "limit=1&sortby=FechaCreacion&order=desc&query=Activo:true,ElementoMovimientoId__Id:" + strconv.Itoa(id)
 }
