5800ff3c312d7ccc98d3900d715a972b093e24b7
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2023-01-19 01:14:00 -0500

    feat udistrital/arka_cliente#1084: Seguridad
    - Limpieza modelos asociados a api movimientos_crud.

--- models/cuentas_grupo.go
+++ models/cuentas_grupo.go
@@ -13,51 +13,6 @@ type CuentaSubgrupo struct {
 	SubgrupoId          *Subgrupo
 }
 
-type CuentasGrupoModelo struct {
-	Id                  int             `orm:"column(id);pk;auto"`
-	CuentaCreditoId     int             `orm:"column(cuenta_credito_id)"`
-	CuentaDebitoId      int             `orm:"column(cuenta_debito_id)"`
-	SubtipoMovimientoId int             `orm:"column(subtipo_movimiento_id)"`
-	FechaCreacion       time.Time       `orm:"auto_now;column(fecha_creacion);type(date)"`
-	FechaModificacion   time.Time       `orm:"auto_now;column(fecha_modificacion);type(date)"`
-	Activo              bool            `orm:"column(activo)"`
-	SubgrupoId          *SubgrupoModelo `orm:"column(subgrupo_id);rel(fk)"`
-}
-
-type CuentasGrupoMovimiento struct {
-	Id              int
-	CuentaCreditoId string
-	CuentaDebitoId  string
-	//SubtipoMovimientoId *TipoMovimiento
-	SubtipoMovimientoId int
-	FechaCreacion       time.Time
-	FechaModificacion   time.Time
-	Activo              bool
-	SubgrupoId          *SubgrupoModelo
-}
-
-type CuentasGrupoTransaccion struct {
-	Id                  int
-	CuentaCreditoId     *CuentaContable
-	CuentaDebitoId      *CuentaContable
-	SubtipoMovimientoId int
-	FechaCreacion       time.Time
-	FechaModificacion   time.Time
-	Activo              bool
-	SubgrupoId          *Subgrupo
-}
-
-type Movimientos_Kronos struct {
-	Id                int
-	Nombre            string
-	Descripcion       string
-	Acronimo          string
-	Activo            bool
-	FechaCreacion     time.Time
-	FechaModificacion time.Time
-	Parametros        string
-}
-
 type TransaccionMovimientos struct {
 	ConsecutivoId    int
 	Etiquetas        string
