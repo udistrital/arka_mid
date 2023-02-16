df2b4b1a7192dbe27430d1c986514d5beae78c86
Ludwing Sanchez <ludwingsanchezleon@gmail.com>
2023-02-16 00:24:38 -0500

    feat udistrital/arka_cliente#1111: Fecha corte movimientos
    - Se ajusta el algoritmo para la contabilización de los traslados teniendo en cuenta el historial de los elementos.

--- helpers/trasladosHelper/AprobarTraslado.go
+++ helpers/trasladosHelper/AprobarTraslado.go
@@ -1,11 +1,12 @@
 package trasladoshelper
 
 import (
-	"net/url"
+	"time"
 
 	"github.com/udistrital/arka_mid/helpers/asientoContable"
 	"github.com/udistrital/arka_mid/helpers/crud/actaRecibido"
 	"github.com/udistrital/arka_mid/helpers/crud/movimientosArka"
+	"github.com/udistrital/arka_mid/helpers/inventarioHelper"
 	"github.com/udistrital/arka_mid/helpers/mid/movimientosContables"
 	"github.com/udistrital/arka_mid/helpers/utilsHelper"
 	"github.com/udistrital/arka_mid/models"
@@ -15,19 +16,16 @@ import (
 // AprobarTraslado Actualiza el estado del traslado y genera la transaccion contable correspondiente
 func AprobarTraslado(id int, response *models.ResultadoMovimiento) (outputError map[string]interface{}) {
 
-	funcion := "AprobarTraslado - "
-	defer errorctrl.ErrorControlFunction(funcion+"Unhandled Error!", "500")
+	defer errorctrl.ErrorControlFunction("AprobarTraslado - Unhandled Error!", "500")
 
 	var (
-		detalle      models.FormatoTraslado
-		tipoSalida   int
-		query        string
-		mapElementos = make(map[int][]models.ElementosMovimiento)
-		transaccion  models.TransaccionMovimientos
+		detalle     models.FormatoTraslado
+		tipoSalida  int
+		transaccion models.TransaccionMovimientos
 	)
 
 	movimiento_, outputError := movimientosArka.GetMovimientoById(id)
-	if outputError != nil {
+	if outputError != nil || movimiento_.EstadoMovimientoId.Nombre != "Traslado Confirmado" {
 		return
 	}
 
@@ -44,62 +42,38 @@ func AprobarTraslado(id int, response *models.ResultadoMovimiento) (outputError 
 		return err
 	}
 
-	query = "limit=-1&fields=Id,ElementoActaId,ValorTotal,MovimientoId&sortby=ElementoActaId&order=desc" +
-		"&query=Id__in:" + url.QueryEscape(utilsHelper.ArrayToString(detalle.Elementos, "|"))
-	if elementos_, err := movimientosArka.GetAllElementosMovimiento(query); err != nil {
-		return err
-	} else if len(elementos_) == len(detalle.Elementos) {
-		for _, el := range elementos_ {
-			mapElementos[el.MovimientoId.MovimientoPadreId.FormatoTipoMovimientoId.Id] = append(mapElementos[el.MovimientoId.MovimientoPadreId.FormatoTipoMovimientoId.Id], *el)
-		}
-	} else {
-		response.Error = "No se pudo consultar la parametrización de los elementos. Contacte soporte"
-		return
-	}
-
 	bufferCuentas := make(map[string]models.CuentaContable)
-	fields := "Id,SubgrupoCatalogoId,TipoBienId,ValorUnitario,ValorTotal"
-	for tipoEntr, el_ := range mapElementos {
+	bufferSubgrupos := make(map[int]models.DetalleSubgrupo)
+	for _, el := range detalle.Elementos {
 
-		var (
-			ids           []int
-			ids_          []int
-			elementosActa []*models.Elemento
-			novedades     map[int]models.NovedadElemento
-		)
-
-		for _, el := range el_ {
-			ids = append(ids, el.Id)
-			ids_ = append(ids_, el.ElementoActaId)
+		historial, err := movimientosArka.GetHistorialElemento(el, true)
+		if err != nil {
+			return err
+		} else if historial == nil {
+			response.Error = "No se pudo la parametrización de los elementos. Contacte soporte."
+			return
 		}
 
-		query = "Id__in:" + utilsHelper.ArrayToString(ids_, "|")
-		elementosActa, outputError = actaRecibido.GetAllElemento(query, fields, "Id", "desc", "", "-1")
+		valor, _, _, _, err := inventarioHelper.GetUltimoValor(*historial)
+		if err != nil {
+			return err
+		}
+
+		if valor <= 0 {
+			continue
+		}
+
+		var elementoActa models.Elemento
+		outputError = actaRecibido.GetElementoById(historial.Elemento.ElementoActaId, &elementoActa)
 		if outputError != nil {
 			return
-		} else if len(elementosActa) != len(el_) {
-			response.Error = "No se pudo consultar la parametrización de los elementos. Contacte soporte"
-			return
 		}
 
-		query = "limit=-1&fields=ElementoMovimientoId,ValorLibros&sortby=MovimientoId,FechaCreacion&order=asc,asc&query=Activo:true,ElementoMovimientoId__Id__in:" +
-			utilsHelper.ArrayToString(ids, "|")
-		if novedades_, err := movimientosArka.GetAllNovedadElemento(query); err != nil {
-			return err
-		} else {
-			novedades = make(map[int]models.NovedadElemento)
-			for _, nov := range novedades_ {
-				novedades[nov.ElementoMovimientoId.ElementoActaId] = *nov
-			}
-		}
+		elementoActa.ValorTotal = valor
+		elementosActa := []*models.Elemento{&elementoActa}
+		tipoEntrada := historial.Salida.MovimientoPadreId.FormatoTipoMovimientoId.Id
 
-		for _, el := range elementosActa {
-			if val, ok := novedades[el.Id]; ok {
-				el.ValorTotal = val.ValorLibros
-			}
-		}
-
-		response.Error, outputError = asientoContable.CalcularMovimientosContables(elementosActa, descMovDestino(), tipoEntr, tipoSalida, detalle.FuncionarioDestino, detalle.FuncionarioOrigen, bufferCuentas, nil, &transaccion.Movimientos)
+		response.Error, outputError = asientoContable.CalcularMovimientosContables(elementosActa, descMovDestino(), tipoEntrada, tipoSalida, detalle.FuncionarioDestino, detalle.FuncionarioOrigen, bufferCuentas, bufferSubgrupos, &transaccion.Movimientos)
 		if outputError != nil || response.Error != "" {
 			return
 		}
@@ -123,6 +97,7 @@ func AprobarTraslado(id int, response *models.ResultadoMovimiento) (outputError 
 		return
 	}
 
+	response.Movimiento.FechaCorte = utilsHelper.Time(time.Now())
 	_, outputError = movimientosArka.PutMovimiento(&response.Movimiento, response.Movimiento.Id)
 
 	return
